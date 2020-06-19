8b31e20d89461c021460f901789e8b8dd226bf73
Rasmus Ølholm Nielsen <erasmus1212@hotmail.com>
2020-06-19 18:07:35 +0200

    export 3d line init

---
+++ PathVisualizer/Export3DLine/Export3DLineModule.cs
@@ -0,0 +1,25 @@
+﻿using Export3DLine.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Export3DLine
+{
+    public class Export3DLineModule : IModule
+    {
+        public Export3DLineModule(IRegionManager regionManager)
+        {
+            regionManager.RegisterViewWithRegion(Lib.RegionNames.MenuRegionFile, typeof(Export3DLineView));
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            containerProvider.Resolve<Export3DLineView>();
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<Export3DLineView>();
+        }
+    }
+}
---
+++ PathVisualizer/Export3DLine/ViewModels/Export3DLineViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Export3DLine.ViewModels
+{
+    public class Export3DLineViewModel : BindableBase
+    {
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
+        public Export3DLineViewModel()
+        {
+            Message = "View A from your Prism Module";
+        }
+    }
+}
